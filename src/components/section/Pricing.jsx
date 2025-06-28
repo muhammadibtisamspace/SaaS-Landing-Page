a6790e279e3fa4f4e1065e75aece2cf2a63c4a4e
muhammadibtisamspace <muhammadibtisamspace@gmail.com>
2025-06-28 11:09:49 +0500

    Update Pricing.jsx

--- src/components/section/Pricing.jsx
+++ src/components/section/Pricing.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Container from "../shared/container";
+import Container from "../shared/Container";
 import Paragraph from "../shared/Paragraph";
 import Title from "../shared/Title";
 import PricingPlan from "../shared/PricingPlan";
